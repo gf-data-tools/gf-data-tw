64fc6a66a2130b04b27cfb57b5949d022e894457
ZeroRin <ZeroRin@users.noreply.github.com>
2023-09-22 02:20:22 +0000

    TW | 30199 | data 4132ddf | dabao 20230921_16_42

--- asset/luapatch/3030/dailygameautoplancontroller.lua
+++ asset/luapatch/3030/dailygameautoplancontroller.lua
@@ -6,5 +6,18 @@ local CheckAutoPlanInDailyGame = function()
 	CS.RewardBoxController.boxes:Clear();
 end
 
-util.hotfix_ex(CS.DailyGameAutoPlanController,'CheckAutoPlanInDailyGame',CheckAutoPlanInDailyGame)
+local MissionState = function(finishHexCount)
+	local count = finishHexCount -1;
+	return CS.DailyGameAutoPlanController.MissionState(count);
+end
 
+local GoToDeployment = function(hexData)
+	if not hexData:isMeetSelect() then
+		CS.DailyGameAutoPlanController.RequestPlanFinish();
+		return;
+	end
+	CS.DailyGameAutoPlanController.GoToDeployment(hexData);
+end
+util.hotfix_ex(CS.DailyGameAutoPlanController,'MissionState',MissionState)
+util.hotfix_ex(CS.DailyGameAutoPlanController,'CheckAutoPlanInDailyGame',CheckAutoPlanInDailyGame)
+util.hotfix_ex(CS.DailyGameAutoPlanController,'GoToDeployment',GoToDeployment)
--- asset/luapatch/3030/deploymentautobattlecontroller.lua
+++ asset/luapatch/3030/deploymentautobattlecontroller.lua
@@ -4,10 +4,12 @@ xlua.private_accessible(CS.DeploymentAutoBattleController)
 local CheckMainTeamAssistTeam = function()
 	CS.DeploymentAutoBattleController.CheckMainTeamAssistTeam();
 	if CS.DeploymentAutoBattleController.currentAutoMoveStep.teamType == CS.AutoBattleTeamType.MainTeam then
-		for i=0,CS.DeploymentAutoBattleController.currentMissionSteps.autoBattleAllSteps.Count-1 do
-			local onestep = CS.DeploymentAutoBattleController.currentMissionSteps.autoBattleAllSteps[i];
-			if not onestep.onlyOnce then
-				CS.DeploymentAutoBattleController.oneStepWaitThisTurn:Remove(onestep.index);
+		if CS.DeploymentAutoBattleController.currentMissionSteps ~= nil then
+			for i=0,CS.DeploymentAutoBattleController.currentMissionSteps.autoBattleAllSteps.Count-1 do
+				local onestep = CS.DeploymentAutoBattleController.currentMissionSteps.autoBattleAllSteps[i];
+				if not onestep.onlyOnce then
+					CS.DeploymentAutoBattleController.oneStepWaitThisTurn:Remove(onestep.index);
+				end
 			end
 		end
 	end
